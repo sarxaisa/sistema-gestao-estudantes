bc62bc18e45fd9d8c025ac66214d0039871e941c
sarxaisa <118470659+sarxaisa@users.noreply.github.com>
2023-08-24 15:23:30 -0300

    24/08
    Ebaaa

---
+++ sistema-gestao-estudantes/AtualizarDeletarEstudante.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace sistema_gestao_estudantes
+{
+    partial class AtualizarDeletarEstudante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ID = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.NOME = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SOBRENOME = new System.Windows.Forms.Label();
+            this.Feminino = new System.Windows.Forms.RadioButton();
+            this.Masculino = new System.Windows.Forms.RadioButton();
+            this.Data = new System.Windows.Forms.DateTimePicker();
+            this.Nascimento = new System.Windows.Forms.Label();
+            this.Gênero = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Telefone = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Endereço = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(50, 33);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(21, 15);
+            this.ID.TabIndex = 0;
+            this.ID.Text = "ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // NOME
+            // 
+            this.NOME.AutoSize = true;
+            this.NOME.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NOME.Location = new System.Drawing.Point(50, 77);
+            this.NOME.Name = "NOME";
+            this.NOME.Size = new System.Drawing.Size(48, 15);
+            this.NOME.TabIndex = 3;
+            this.NOME.Text = "NOME";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(151, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // SOBRENOME
+            // 
+            this.SOBRENOME.AutoSize = true;
+            this.SOBRENOME.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SOBRENOME.Location = new System.Drawing.Point(50, 129);
+            this.SOBRENOME.Name = "SOBRENOME";
+            this.SOBRENOME.Size = new System.Drawing.Size(95, 15);
+            this.SOBRENOME.TabIndex = 5;
+            this.SOBRENOME.Text = "SOBRENOME";
+            // 
+            // Feminino
+            // 
+            this.Feminino.AutoSize = true;
+            this.Feminino.Checked = true;
+            this.Feminino.Location = new System.Drawing.Point(15, 37);
+            this.Feminino.Name = "Feminino";
+            this.Feminino.Size = new System.Drawing.Size(67, 17);
+            this.Feminino.TabIndex = 7;
+            this.Feminino.TabStop = true;
+            this.Feminino.Text = "Feminino";
+            this.Feminino.UseVisualStyleBackColor = true;
+            // 
+            // Masculino
+            // 
+            this.Masculino.AutoSize = true;
+            this.Masculino.Location = new System.Drawing.Point(88, 37);
+            this.Masculino.Name = "Masculino";
+            this.Masculino.Size = new System.Drawing.Size(73, 17);
+            this.Masculino.TabIndex = 8;
+            this.Masculino.Text = "Masculino";
+            this.Masculino.UseVisualStyleBackColor = true;
+            // 
+            // Data
+            // 
+            this.Data.Location = new System.Drawing.Point(151, 163);
+            this.Data.Name = "Data";
+            this.Data.Size = new System.Drawing.Size(200, 20);
+            this.Data.TabIndex = 9;
+            // 
+            // Nascimento
+            // 
+            this.Nascimento.AutoSize = true;
+            this.Nascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nascimento.Location = new System.Drawing.Point(50, 163);
+            this.Nascimento.Name = "Nascimento";
+            this.Nascimento.Size = new System.Drawing.Size(83, 15);
+            this.Nascimento.TabIndex = 10;
+            this.Nascimento.Text = "Nascimento";
+            // 
+            // Gênero
+            // 
+            this.Gênero.AutoSize = true;
+            this.Gênero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gênero.Location = new System.Drawing.Point(50, 208);
+            this.Gênero.Name = "Gênero";
+            this.Gênero.Size = new System.Drawing.Size(54, 15);
+            this.Gênero.TabIndex = 11;
+            this.Gênero.Text = "Gênero";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Feminino);
+            this.groupBox1.Controls.Add(this.Masculino);
+            this.groupBox1.Location = new System.Drawing.Point(151, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Telefone
+            // 
+            this.Telefone.AutoSize = true;
+            this.Telefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Telefone.Location = new System.Drawing.Point(50, 330);
+            this.Telefone.Name = "Telefone";
+            this.Telefone.Size = new System.Drawing.Size(63, 15);
+            this.Telefone.TabIndex = 14;
+            this.Telefone.Text = "Telefone";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(132, 330);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(631, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Endereço
+            // 
+            this.Endereço.AutoSize = true;
+            this.Endereço.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Endereço.Location = new System.Drawing.Point(51, 373);
+            this.Endereço.Name = "Endereço";
+            this.Endereço.Size = new System.Drawing.Size(68, 15);
+            this.Endereço.TabIndex = 17;
+            this.Endereço.Text = "Endereço";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(133, 373);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 18;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(643, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Enviar Foto";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 365);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Remover";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(329, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Procurar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(571, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Foto";
+            // 
+            // AtualizarDeletarEstudante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Endereço);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Telefone);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Gênero);
+            this.Controls.Add(this.Nascimento);
+            this.Controls.Add(this.Data);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.SOBRENOME);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.NOME);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ID);
+            this.Name = "AtualizarDeletarEstudante";
+            this.Text = "AtualizarDeletarEstudante";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label NOME;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label SOBRENOME;
+        private System.Windows.Forms.RadioButton Feminino;
+        private System.Windows.Forms.RadioButton Masculino;
+        private System.Windows.Forms.DateTimePicker Data;
+        private System.Windows.Forms.Label Nascimento;
+        private System.Windows.Forms.Label Gênero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Telefone;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Endereço;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
